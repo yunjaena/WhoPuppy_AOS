8b3a275e0b9b76985dd68b9001c2928364951dc7
Yunjae-Na <yunjaena@gmail.com>
2021-03-19 19:15:32 +0900

    Add Ktlint
    Add Ktlint

---
+++ app/src/main/java/com/yunjaena/whopuppy/base/activity/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.yunjaena.whopuppy.base.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+open class BaseActivity : AppCompatActivity() {
+    protected val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
