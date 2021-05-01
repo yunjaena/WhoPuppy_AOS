cb0e183bb8889b1e6c9d7d8445e48701b2958bb1
Yunjae-Na <yunjaena@gmail.com>
2021-05-01 18:12:00 +0900

    Add main activity
    Add main activity

---
+++ app/src/main/java/com/yunjaena/whopuppy/activity/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.yunjaena.whopuppy.activity
+
+import com.yunjaena.whopuppy.R
+import com.yunjaena.whopuppy.base.activity.ViewBindingActivity
+import com.yunjaena.whopuppy.databinding.ActivityMainBinding
+
+class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
+    override val layoutId: Int = R.layout.activity_main
+}
