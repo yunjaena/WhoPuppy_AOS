62f2dc37970eb6bd98fa877bb4affe70f91a6f7b
Yunjae-Na <yunjaena@gmail.com>
2021-05-04 20:04:00 +0900

    Add loading dialog at base activity
    Add loading dialog at base activity

--- app/src/main/java/com/yunjaena/whopuppy/base/activity/BaseActivity.kt
+++ app/src/main/java/com/yunjaena/whopuppy/base/activity/BaseActivity.kt
@@ -1,16 +1,21 @@
 package com.yunjaena.whopuppy.base.activity
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.yunjaena.whopuppy.R
 import com.yunjaena.whopuppy.base.appbar.BaseAppBar
+import com.yunjaena.whopuppy.base.hideProgressDialog
+import com.yunjaena.whopuppy.base.showProgressDialog
+import com.yunjaena.whopuppy.base.viewmodel.ViewModelBase
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 open class BaseActivity : AppCompatActivity() {
     protected val compositeDisposable = CompositeDisposable()
     private var baseAppBar: BaseAppBar? = null
+    private var progressDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +50,29 @@ open class BaseActivity : AppCompatActivity() {
         baseAppBar?.setLeftButtonDrawable(backButtonDrawable)
         appBarLeftButtonClicked {
             onBackPressed()
+        }
+    }
+
+    fun useDefaultLoading(viewModelBase: ViewModelBase, useDim: Boolean = true) {
+        viewModelBase.isLoading.observe(this) { isLoading ->
+            when (isLoading) {
+                true -> showProgress(useDim)
+                false -> hideProgress()
+            }
+        }
+    }
+
+    fun showProgress(useDim: Boolean = true) {
+        runOnUiThread {
+            progressDialog?.hideProgressDialog()
+            progressDialog = this@BaseActivity.showProgressDialog(useDim)
+        }
+    }
+
+    fun hideProgress() {
+        runOnUiThread {
+            progressDialog?.hideProgressDialog()
+            progressDialog = null
         }
     }
 
