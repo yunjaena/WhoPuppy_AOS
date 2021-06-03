a6df5e45122b2732a07658a16e130d92ad5605f8
Yunjae-Na <yunjaena@gmail.com>
2021-06-03 20:41:00 +0900

    Add auto login function
    Add auto login function

--- app/src/main/java/com/yunjaena/whopuppy/activity/SplashActivity.kt
+++ app/src/main/java/com/yunjaena/whopuppy/activity/SplashActivity.kt
@@ -2,7 +2,6 @@ package com.yunjaena.whopuppy.activity
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
 import com.yunjaena.whopuppy.R
 import com.yunjaena.whopuppy.base.activity.ViewBindingActivity
 import com.yunjaena.whopuppy.databinding.ActivitySplashBinding
@@ -10,11 +9,12 @@ import com.yunjaena.whopuppy.viewmodel.SplashViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.kotlin.addTo
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.util.concurrent.TimeUnit
 
 class SplashActivity : ViewBindingActivity<ActivitySplashBinding>() {
     override val layoutId: Int = R.layout.activity_splash
-    private val splashViewModel: SplashViewModel by viewModels()
+    private val splashViewModel: SplashViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +22,6 @@ class SplashActivity : ViewBindingActivity<ActivitySplashBinding>() {
     }
 
     private fun init() {
-        // TODO -> 자동 로그인
         initObserver()
         Observable.timer(2L, TimeUnit.SECONDS)
             .take(1)
--- app/src/main/java/com/yunjaena/whopuppy/di/ViewModelModule.kt
+++ app/src/main/java/com/yunjaena/whopuppy/di/ViewModelModule.kt
@@ -1,11 +1,15 @@
 package com.yunjaena.whopuppy.di
 
+import com.yunjaena.whopuppy.viewmodel.FindPasswordViewModel
+import com.yunjaena.whopuppy.viewmodel.LoginViewModel
 import com.yunjaena.whopuppy.viewmodel.SignUpViewModel
 import com.yunjaena.whopuppy.viewmodel.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { SplashViewModel() }
+    viewModel { SplashViewModel(get()) }
     viewModel { SignUpViewModel(get()) }
+    viewModel { LoginViewModel(get()) }
+    viewModel { FindPasswordViewModel(get()) }
 }
